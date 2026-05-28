c8c74514f7c676fc81a6b7842914b72704e78eff
syedadaman19-stack <281551519+syedadaman19-stack@users.noreply.github.com>
2026-05-29 00:07:36 +0500

    Add files via upload

---
+++ week 8.py
@@ -0,0 +1,9 @@
+# task 1
+print("    *  ")
+print("  *   * ")
+print(" *     *")
+print("* * * * * ")
+#task 2 
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(celsius, "=", fahrenheit)
